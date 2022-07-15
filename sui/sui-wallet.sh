078ce751712f2dc3cbd58489c725980c6042c1f1
lukmanc405 <lukmanc405@gmail.com>
2022-07-15 11:45:02 +0700

    Update sui-wallet.sh

--- sui/sui-wallet.sh
+++ sui/sui-wallet.sh
@@ -1,8 +1,32 @@
+#!/bin/bash
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " | |  | | | | |/ /  \/  |  / \  | \ | |  ";
+echo " | |  | | | | ' /| |\/| | / _ \ |  \| |  ";
+echo " | |__| |_| | . \| |  | |/ ___ \| |\  |  ";
+echo " |_____\___/|_|\_\_|  |_/_/   \_\_| \_|  ";
+echo -e "\e[0m"
+echo "=================================================="  
+
+sleep 2
+# Install Binaries && cargo
 apt install cargo -y
-
 sudo curl https://sh.rustup.rs/ -sSf | sh -s -- -y
-source $HOME/.cargo/env
-
-cargo install --locked --git https://github.com/MystenLabs/sui.git --branch "devnet" sui
+source $HOME/.cargo/env && cargo install --locked --git https://github.com/MystenLabs/sui.git --branch "devnet" sui
 
 sleep 1
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+echo $PATH
+
+sleep 1
+
+# switch gateway
+
+cd sui && wallet
+
+wallet switch --gateway https://gateway.devnet.sui.io:443
+
+echo -e "\e[1m\e[32mSui Wallet Created \e[0m"
