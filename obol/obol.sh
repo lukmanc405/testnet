685f756dc749665abfb31e450b178950451b871e
lukmanc405 <lukmanc405@gmail.com>
2022-07-21 13:23:05 +0700

    Update obol.sh

--- obol/obol.sh
+++ obol/obol.sh
@@ -29,41 +29,22 @@ sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-r
     libclang-dev \
     cmake
 
-sleep 1
-
-# install docker
+echo -e "\e[1m\e[32m3. install docker... \e[0m" && sleep 1
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-sleep 1
 
-# install docker compose
+echo -e "\e[1m\e[32m4. install docker compose... \e[0m" && sleep 1
 docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
 sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
 sudo chmod +x /usr/bin/docker-compose
 
-sleep 1
+echo -e "\e[1m\e[32m5. clone repo and change dir permission... \e[0m" && sleep 1
+git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git && chmod o+w charon-distributed-validator-node
 
-# Clone repo
-git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
-
-sleep 1
-
-# Change the directory permissions
-chmod o+w charon-distributed-validator-node
-
-sleep 1
-
-# Change directory
-cd charon-distributed-validator-node
-
-sleep 1
-
-# Create your charon ENR private key, this will create a charon-enr-private-key file in the .charon directory
+echo -e "\e[1m\e[32m6. create your charon ENR private key, this will create a charon-enr-private-key file in the .charon directory
 docker run --rm -v "$(pwd):/opt/charon" ghcr.io/obolnetwork/charon:v0.8.0 create enr
-
-sleep 1
 
