2224ababb075f96036b5ce71dc94812b19f4ed00
lukmanc405 <lukmanc405@gmail.com>
2022-07-21 13:14:59 +0700

    create obol

---
+++ obol/obol.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " | |  | | | | |/ /  \/  |  / \  | \ | |  ";
+echo " | |  | | | | ' /| |\/| | / _ \ |  \| |  ";
+echo " | |__| |_| | . \| |  | |/ ___ \| |\  |  ";
+echo " |_____\___/|_|\_\_|  |_/_/   \_\_| \_|  ";
+echo -e "\e[0m"
+echo "=================================================="  
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+sudo apt-get install jq -y
+sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
+    tzdata \
+    git \
+    ca-certificates \
+    curl \
+    build-essential \
+    libssl-dev \
+    pkg-config \
+    libclang-dev \
+    cmake
+
+sleep 1
+
+# install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sleep 1
+
+# install docker compose
+docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+sudo chmod +x /usr/bin/docker-compose
+
+sleep 1
+
+# Clone repo
+git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
+
+sleep 1
+
+# Change the directory permissions
+chmod o+w charon-distributed-validator-node
+
+sleep 1
+
+# Change directory
+cd charon-distributed-validator-node
+
+sleep 1
+
+# Create your charon ENR private key, this will create a charon-enr-private-key file in the .charon directory
+docker run --rm -v "$(pwd):/opt/charon" ghcr.io/obolnetwork/charon:v0.8.0 create enr
+
+sleep 1
+
