bf1ee972ef833ec8bd8be7b9a2da202f2828f40f
lukmanc405 <lukmanc405@gmail.com>
2022-07-24 09:46:36 +0700

    Update update.sh

--- sui/tools/update.sh
+++ sui/tools/update.sh
@@ -5,7 +5,5 @@ wget -qO docker-compose.yaml https://raw.githubusercontent.com/MystenLabs/sui/ma
 wget -qO fullnode-template.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
 wget -qO genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 sed -i 's/127.0.0.1/0.0.0.0/' fullnode-template.yaml
-version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
-wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
 docker-compose down --volumes
 docker-compose up -d
