27f77a8e90d61c2eb180d183cbba8956f8f5f2e1
lukmanc405 <lukmanc405@gmail.com>
2022-07-24 11:02:54 +0700

    Update sui-wallet.sh

--- sui/sui-wallet.sh
+++ sui/sui-wallet.sh
@@ -21,16 +21,6 @@ wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux
 
 sleep 1
 
-# download and update configs
-mkdir -p $HOME/.sui
-wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
-wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-yq -i ".db-path = \"$HOME/.sui/db\"" $HOME/.sui/fullnode.yaml
-yq -i '.metrics-address = "0.0.0.0:9184"' $HOME/.sui/fullnode.yaml
-yq -i '.json-rpc-address = "0.0.0.0:9000"' $HOME/.sui/fullnode.yaml
-yq -i ".genesis.genesis-file-location = \"$HOME/.sui/genesis.blob\"" $HOME/.sui/fullnode.yaml
-
-
 echo -e "\e[1m\e[32m2. Updating packages... \e[0m" && sleep 1
 # update
 sudo apt update && sudo apt upgrade -y
