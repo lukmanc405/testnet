d69dc7bc4c924fc3f6019c3d265e24d58281a458
lukmanc405 <lukmanc405@gmail.com>
2022-07-25 16:25:51 +0700

    Update sui-wallet.sh

--- sui/sui-wallet.sh
+++ sui/sui-wallet.sh
@@ -10,15 +10,19 @@ echo "=================================================="
 
 sleep 2
 
-echo -e "\e[1m\e[32m1. Installing dependencies... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Checking dependencies... \e[0m" && sleep 1
 # packages
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
-sudo apt-get install jq -y 
+if [ -x /usr/local/bin/yq ] then
+    echo -e "\e[1m\e[32m1.1 dependencies already installed \e[0m" && sleep 1
+else
+    echo -e "\e[1m\e[32m1.1 Installing dependencies ... \e[0m" && sleep 1
+    sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+    sudo apt-get install jq -y 
 
 
 echo -e "\e[1m\e[32m2. Checking sui binaries \e[0m" && sleep 1
 if sui --version >/dev/null 2>&1; then
-    echo -e "\e[1m\e[32m2.1 wallet already installed \e[0m" && sleep 2
+    echo -e "\e[1m\e[32m2.1 Wallet already installed \e[0m" && sleep 2
 else
     echo -e "\e[1m\e[32m2.1 Installing sui binaries \e[0m" && sleep 1
     version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
