11593d6268c614a67535409d138e98a1315aa0a8
lukmanc405 <lukmanc405@gmail.com>
2022-07-25 14:48:46 +0700

    Update sui-wallet.sh

--- sui/sui-wallet.sh
+++ sui/sui-wallet.sh
@@ -15,9 +15,14 @@ echo -e "\e[1m\e[32m1. Installing dependencies... \e[0m" && sleep 1
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
 sudo apt-get install jq -y 
 
-# download sui binaries
-version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
-wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
+
+echo -e "\e[1m\e[32m2. Checking sui binaries \e[0m" && sleep 1 
+sui --version
+if [ $? -ne 0 ]
+then
+    echo -e "\e[1m\e[32m2.1 Installing sui binaries ... \e[0m" && sleep 1
+    version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+    wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
 
 sleep 1
 
