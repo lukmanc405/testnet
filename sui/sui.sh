6c6976d3933d359e4db0bfedcf2a93d48a885466
lukmanc405 <lukmanc405@gmail.com>
2022-07-27 21:10:13 +0700

    Update sui.sh

--- sui/sui.sh
+++ sui/sui.sh
@@ -10,60 +10,39 @@ echo "=================================================="
 
 sleep 2
 
-echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Updating paket... \e[0m" && sleep 1
 # update
 sudo apt update && sudo apt upgrade -y
 
-echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
-# packages
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
-sudo apt-get install jq -y
-sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
-    tzdata \
-    git \
-    ca-certificates \
-    curl \
-    build-essential \
-    libssl-dev \
-    pkg-config \
-    libclang-dev \
-    cmake
+echo -e "\e[1m\e[32m2. Install paket yang diperlukan... \e[0m" && sleep 1
+sudo apt install wget jq bc build-essential -y
 
-# install docker
+echo -e "\e[1m\e[32m3. Install docker... \e[0m" && sleep 1
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# install docker compose
+echo -e "\e[1m\e[32m4. Install docker compose... \e[0m" && sleep 1
 docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
 sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
 sudo chmod +x /usr/bin/docker-compose
 
-# download configs
-cd $HOME && rm -rf sui
-mkdir sui && cd sui
-wget -qO docker-compose.yaml https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
-wget -qO fullnode-template.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
-wget -qO genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-mv fullnode-template.yaml fullnode.yaml
-sed -i 's/127.0.0.1/0.0.0.0/' fullnode-template.yaml
-docker-compose down --volumes
+echo -e "\e[1m\e[32m5. Download file genesis.. \e[0m" && sleep 1
+mkdir -p $HOME/.sui
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 
-# start application
-docker-compose up -d
+echo -e "\e[1m\e[32m6. Download node config... \e[0m" && sleep 1
+wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
 
-echo "=================================================="
+echo -e "\e[1m\e[32m7. Edit config... \e[0m" && sleep 1
+sed -i -e "s%db-path:.*%db-path: \"$HOME/.sui/db\"%; "\
+"s%metrics-address:.*%metrics-address: \"0.0.0.0:9184\"%; "\
+"s%json-rpc-address:.*%json-rpc-address: \"0.0.0.0:9000\"%; "\
+"s%genesis-file-location:.*%genesis-file-location: \"$HOME/.sui/genesis.blob\"%; " $HOME/.sui/fullnode.yaml
 
-echo -e "\e[1m\e[32mSui FullNode Started \e[0m"
-
-echo "=================================================="
-
-read -p "Do you want to install wallet? (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-  echo -e "\e[1m\e[32m Glad to hear it: \e[0m"
-  wget -O sui-wallet.sh https://raw.githubusercontent.com/lukmanc405/testnet/main/sui/sui-wallet.sh && chmod +x sui-wallet.sh && ./sui-wallet.sh
-else
-   echo -e "\e[1m\e[32m Its okay no problem at all : \e[0m"
-fi
+echo -e "\e[1m\e[32m8. Run cointainer... \e[0m" && sleep 1
+docker run -dit --name sui_node --restart always -u 0:0 \
+  --network host -v $HOME/.sui:/root/.sui secord/sui \
+  --config-path $HOME/.sui/fullnode.yaml
