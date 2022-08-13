45b2a357f13969dca5b0d3aa06eeb89a34f736c1
lukmanc405 <lukmanc405@gmail.com>
2022-08-13 19:12:01 +0700

    Update obol_update.sh

--- obol/tools/obol_update.sh
+++ obol/tools/obol_update.sh
@@ -1,4 +1,4 @@
 cd $HOME/charon-distributed-validator-node/ && docker-compose down
 rm -rf docker-compose.yml
-wget -qO $HOME/charon-distributed-validator-node/docker-compose.yml https://raw.githubusercontent.com/lukmanc405/testnet/main/obol/docker-compose.yml
+wget -qO $HOME/charon-distributed-validator-node/docker-compose.yml https://raw.githubusercontent.com/ObolNetwork/charon-distributed-validator-node/main/docker-compose.yml
 git pull && docker-compose up -d
