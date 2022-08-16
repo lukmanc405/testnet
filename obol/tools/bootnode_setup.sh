b595ed2b7623a2483cd269dd7e1c55e3768b88e7
lukmanc405 <lukmanc405@gmail.com>
2022-08-16 19:27:25 +0700

    Update bootnode_setup.sh

--- obol/tools/bootnode_setup.sh
+++ obol/tools/bootnode_setup.sh
@@ -1,10 +1,7 @@
 echo -e "\e[1m\e[32m [1]. open permission... \e[0m" && sleep 1
 cd $HOME/charon-distributed-validator-node/ && sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
 sudo chmod -R 666 .charon/validator_keys && sudo chmod a+rwx .charon/validator_keys
-echo -e "\e[1m\e[32m [2]. mengahapus file keystore-0.json.lock... \e[0m" && sleep 1
-sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
-rm -rf .charon/validator_keys/keystore-0.json.lock
-echo -e "\e[1m\e[32m [3]. setting file docker-compose.yaml & deposit-data.json... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m [2]. setting file docker-compose.yaml & deposit-data.json... \e[0m" && sleep 1
 docker-compose down
 rm -rf $HOME/charon-distributed-validator-node/docker-compose.yaml
 rm -rf $HOME/charon-distributed-validator-node/.charon/deposit-data.json
@@ -13,4 +10,4 @@ wget -qO $HOME/charon-distributed-validator-node/.charon/deposit-data.json https
 sudo chmod -R 666 .charon
 sudo chmod a+rwx .charon
 docker-compose up -d
-echo -e "\e[1m\e[32m [4]. selesai... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m -selesai- \e[0m" && sleep 1
