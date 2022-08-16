844c651ed19f69473efcbd42518aa9520264246b
lukmanc405 <lukmanc405@gmail.com>
2022-08-16 09:40:05 +0700

    Update bootnode_setup.sh

--- obol/tools/bootnode_setup.sh
+++ obol/tools/bootnode_setup.sh
@@ -5,7 +5,7 @@ echo -e "\e[1m\e[32m2. mengahapus file keystore-0.json.lock... \e[0m" && sleep 1
 sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
 rm -rf .charon/validator_keys/keystore-0.json.lock
 echo -e "\e[1m\e[32m3. update versi charon terbaru... \e[0m" && sleep 1
-cd $HOME/charon-distributed-validator-node/ && sudo chmod a+rwx .charon && sudo chmod -R 666 .charon && docker run --rm -v "$(pwd):/opt/charon" ghcr.io/obolnetwork/charon:v0.9.0 enr
+sudo chmod a+rwx .charon && sudo chmod -R 666 .charon && docker run --rm -v "$(pwd):/opt/charon" ghcr.io/obolnetwork/charon:v0.9.0 enr
 echo -e "\e[1m\e[32m4. setting file docker-compose.yaml & deposit-data.json... \e[0m" && sleep 1
 docker-compose down
 sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
