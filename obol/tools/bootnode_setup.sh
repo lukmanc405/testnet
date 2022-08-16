a2e4aebf6333615eee1a170e862dd4318abd09a6
lukmanc405 <lukmanc405@gmail.com>
2022-08-16 10:48:46 +0700

    Update bootnode_setup.sh

--- obol/tools/bootnode_setup.sh
+++ obol/tools/bootnode_setup.sh
@@ -6,10 +6,11 @@ sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
 rm -rf .charon/validator_keys/keystore-0.json.lock
 echo -e "\e[1m\e[32m [3]. setting file docker-compose.yaml & deposit-data.json... \e[0m" && sleep 1
 docker-compose down
-sudo chmod a+rwx .charon && sudo chmod -R 666 .charon
 rm -rf $HOME/charon-distributed-validator-node/docker-compose.yaml
 rm -rf $HOME/charon-distributed-validator-node/.charon/deposit-data.json
 wget -qO $HOME/charon-distributed-validator-node/docker-compose.yml https://raw.githubusercontent.com/lukmanc405/testnet/main/obol/docker-compose.yml
 wget -qO $HOME/charon-distributed-validator-node/.charon/deposit-data.json https://raw.githubusercontent.com/lukmanc405/testnet/main/obol/deposit-data.json
+sudo chmod -R 666 .charon
+sudo chmod a+rwx .charon
 docker-compose up -d
 echo -e "\e[1m\e[32m [4]. selesai... \e[0m" && sleep 1
