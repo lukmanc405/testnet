68634b57a2875db2a00ff2698ba1eb604a65263e
lukmanc405 <lukmanc405@gmail.com>
2022-08-18 18:27:36 +0700

    Update aptos-bylukman.sh

--- aptos/aptos-bylukman.sh
+++ aptos/aptos-bylukman.sh
@@ -34,6 +34,7 @@ sudo apt update && sudo apt upgrade -y
 
 echo -e "\e[1m\e[32m [2]. Checking dependencies... \e[0m" && sleep 1
 if ! command  jq –version &> /dev/null
+then
     echo -e "\e[1m\e[32m [2]. Installing dependencies... \e[0m" && sleep 1
     # packages
     sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
