3f75ee8832878dfec46e9dd8bb1ad255929cae23
lukmanc405 <lukmanc405@gmail.com>
2023-02-16 20:45:22 +0700

    update go

--- utils/install_go.sh
+++ utils/install_go.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-while getopts v: flag; do
-  case "${flag}" in
-  v) VER=$OPTARG ;;
-  *) echo "WARN: unknown parameter: ${OPTARG}"
-  esac
-done
-
-version=${VER:-"1.20"}
-
-curl -L -# -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
-rm "go$version.linux-amd64.tar.gz"
-
-touch $HOME/.bash_profile
-source $HOME/.bash_profile
-PATH_INCLUDES_GO=$(grep "$HOME/go/bin" $HOME/.bash_profile)
-if [ -z "$PATH_INCLUDES_GO" ]; then
-  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-  echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
-fi
+sudo rm -rf /usr/local/go;
+curl https://dl.google.com/go/go1.19.2.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf - ;
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+source $HOME/.profile
