eebd1c0c3e7e5a86f3b61ff87425b85b559f98c3
lukmanc405 <lukmanc405@gmail.com>
2023-02-23 12:30:30 +0700

    edit command

--- ironfish/fix_db.sh
+++ ironfish/fix_db.sh
@@ -3,8 +3,10 @@ rm ~/.ironfish/databases/chain/LOCK
 sleep 1
 wget -O hosts.json https://raw.githubusercontent.com/lukmanc405/testnet/main/ironfish/hosts.json
 mv hosts.json ./.ironfish/hosts.json
+sleep 2
 ironfish migrations:start
+sleep 1m
 systemctl stop ironfishd && systemctl restart ironfishd
 ironfish config:set enableTelemetry true
-sleep 2
+sleep 1m
 ironfish status -f
